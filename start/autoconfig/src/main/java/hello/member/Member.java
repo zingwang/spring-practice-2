69bf42cce8549a2f27aa7113794a7b1b907f8470
zingwang <zingwang06@naver.com>
2023-04-09 11:16:37 +0900

    자동구성(Auto configuration) - 예제 생성

---
+++ start/autoconfig/src/main/java/hello/member/Member.java
@@ -0,0 +1,13 @@
+package hello.member;
+import lombok.Data;
+@Data
+public class Member {
+    private String memberId;
+    private String name;
+    public Member() {
+    }
+    public Member(String memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
+}
