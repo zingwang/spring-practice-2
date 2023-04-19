ecb3993321281ce4d40425b13c2c92aef7fa55af
zingwang <zingwang06@naver.com>
2023-04-19 22:54:25 +0900

    외부설정 - OS 환경변수

---
+++ start/external/src/test/java/hello/external/OsEnv.java
@@ -0,0 +1,21 @@
+package hello.external;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+
+@Slf4j
+public class OsEnv {
+
+    public static void main(String[] args){
+        Map<String,String> envMap= System.getenv();
+        for(String key : envMap.keySet()){
+            log.info("env {}={}", key, System.getenv(key));
+        }
+        // example
+        // DBURL=dev.db.com 개발서버
+        // DBURL=prod.db.com 운영서버
+        String dburl=System.getenv("DBURL");
+        
+    }
+}
