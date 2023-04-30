b32680a2c3fb5efe258a4116dfe4e5201e6d27de
zingwang <zingwang06@naver.com>
2023-04-30 22:40:12 +0900

    외부설정 사용 - ConfigurationProerties

--- start/external-read/src/main/java/hello/datasource/MyDataSource.java
+++ start/external-read/src/main/java/hello/datasource/MyDataSource.java
@@ -1,12 +1,20 @@
 package hello.datasource;
-
-@Sl4j
-public class MyDataSource{
-
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import java.time.Duration;
+import java.util.List;
+@Slf4j
+@Data
+public class MyDataSource {
     private String url;
     private String username;
-
-    public MyDataSource(String url, String username, String password, int maxConnection, Duration timeout, List<String> options) {
+    private String password;
+    private int maxConnection;
+    private Duration timeout;
+    private List<String> options;
+    public MyDataSource(String url, String username, String password, int
+            maxConnection, Duration timeout, List<String> options) {
         this.url = url;
         this.username = username;
         this.password = password;
@@ -14,20 +22,13 @@ public class MyDataSource{
         this.timeout = timeout;
         this.options = options;
     }
-
-    private String password;
-    private int maxConnection;
-    private Duration timeout;
-    private List<String> options;
-
     @PostConstruct
-    public void init(){
-        log.info("url={}",url);
-        log.info("username={}",username);
-        log.info("password={}",password);
-        log.info("maxConnection={}",maxConnection);
-        log.info("timeout={}",timeout);
-        log.info("options={}",options);
+    public void init() {
+        log.info("url={}", url);
+        log.info("username={}", username);
+        log.info("password={}", password);
+        log.info("maxConnection={}", maxConnection);
+        log.info("timeout={}", timeout);
+        log.info("options={}", options);
     }
-
 }
