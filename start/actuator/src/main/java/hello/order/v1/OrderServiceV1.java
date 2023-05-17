ab551b880fbc1d6c5bbe2c1089cde22ad3cd1d44
Jay <jayyou@carrotglobal.com>
2023-05-17 14:15:12 +0900

    모니터링 메트릭활용 - 카운터 메트릭 등록

---
+++ start/actuator/src/main/java/hello/order/v1/OrderServiceV1.java
@@ -0,0 +1,45 @@
+package hello.order.v1;
+
+import hello.order.OrderService;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.prometheus.client.Counter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class OrderServiceV1 implements OrderService {
+
+    private AtomicInteger stock = new AtomicInteger(100);
+    private final MeterRegistry registry;
+    public OrderServiceV1(MeterRegistry registry ){
+        this.registry = registry;
+    }
+    @Override
+    public void order() {
+        log.info("주문");
+        stock.decrementAndGet();
+        Counter.Builder("my.order")
+                .tag("class", this.getClass().getName())
+                .tage("method","order")
+                .description("order")
+                .register(registry).increment();
+
+    }
+
+    @Override
+    public void cancel() {
+        log.info("취소");
+        stock.incrementAndGet();
+        Counter.Builder("my.order")
+                .tag("class", this.getClass().getName())
+                .tage("method","cancel")
+                .description("order")
+                .register(registry).increment();
+    }
+
+    @Override
+    public AtomicInteger getStorck() {
+        return stock;
+    }
+}
