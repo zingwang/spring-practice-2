cee8312a6f424dc6eabce773477cedcac1e25b32
Jay <jayyou@carrotglobal.com>
2023-05-19 17:35:10 +0900

    모니터링 메트릭 활용 - 메트릭등록2-@Counted

--- start/actuator/src/main/java/hello/order/v2/OrderServiceV2.java
+++ start/actuator/src/main/java/hello/order/v2/OrderServiceV2.java
@@ -1,29 +1,30 @@
-package hello.order.v1;
+package hello.order.v2;
 
 import hello.order.OrderService;
+import io.micrometer.core.annotation.Counted;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.prometheus.client.Counter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
-public class OrderServiceV1 implements OrderService {
+public class OrderServiceV2 implements OrderService {
 
     private AtomicInteger stock = new AtomicInteger(100);
-    private final MeterRegistry registry;
-    public OrderServiceV1(MeterRegistry registry ){
-        this.registry = registry;
+
+    public OrderServiceV2( ){
+
     }
+
     @Override
+    @Counted("my.order")
     public void order() {
         log.info("주문");
         stock.decrementAndGet();
-
-
     }
 
     @Override
+    @Counted("my.order")
     public void cancel() {
         log.info("취소");
         stock.incrementAndGet();
